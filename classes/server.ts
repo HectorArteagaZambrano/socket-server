0ed966893f900ace85ac86444bc0c27de0b0bf92
Hector Arteaga <hector12arteaga@gmail.com>
2019-07-10 21:29:26 -0500

    Fin sección 5

--- classes/server.ts
+++ classes/server.ts
@@ -31,12 +31,16 @@ export default class Server{
         private escucharSockets(){
             console.log('Escuchando conexiones');
             this.io.on('connection', cliente =>{
-                console.log('Cliente conectado');
                 
+                //Conectar cliente
+                socket.conectarCliente( cliente );
+                //Configurar usuario
+                 socket.configurarUsuario( cliente, this.io);
                 //Mensajes
                 socket.mensaje( cliente, this.io );
                 ///Desconectar
                 socket.desconectar(cliente);
+               
                 
             });
         }
--- sockets/sockets.ts
+++ sockets/sockets.ts
@@ -1,9 +1,21 @@
 import { Socket } from "socket.io";
+import { UsuarioLista } from '../classes/usuarios-lista';
+import { Usuario } from '../classes/usuario';
 
+
+export const UsuariosConectados = new UsuarioLista();
+
+export const conectarCliente = (cliente: Socket) =>{
+    const usuario = new Usuario( cliente.id);
+    UsuariosConectados.agregar( usuario );
+
+}
 
 export const desconectar = (cliente: Socket) =>{
   cliente.on('disconnect', () => {
     console.log('Cliente desconectado');
+
+    UsuariosConectados.borrarUsuario(cliente.id);
   });
 }
 
@@ -13,5 +25,18 @@ export const mensaje = (cliente: Socket, io: SocketIO.Server) =>{
         console.log('Mensaje recibido', payload);
 
         io.emit('mensaje-nuevo', payload );
+    });
+}
+
+//Configurar usuario
+export const configurarUsuario = (cliente: Socket, io: SocketIO.Server) =>{
+    cliente.on('configurar-usuario', ( payload: {nombre:string}, callback: Function)=>{
+       
+        UsuariosConectados.actualizarNombre( cliente.id, payload.nombre);
+
+        callback({
+            ok:true,
+            mensaje: `Usuario ${ payload.nombre }, configurando`
+        });
     });
 }
